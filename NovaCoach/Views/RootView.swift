631cdf37e4cc4b0e55b0bc5924bb76c8565773de
cakirizm <cakir.dubai@gmail.com>
2026-09-03 02:05:46 +0300

    refactor: connect auth onboarding and main app flows

--- NovaCoach/Views/RootView.swift
+++ NovaCoach/Views/RootView.swift
@@ -2,13 +2,15 @@ import SwiftUI
 
 struct RootView: View {
     @EnvironmentObject var store: AppStore
+
     var body: some View {
         Group {
             if !store.isLoggedIn { LoginView() }
             else if !store.onboardingCompleted { OnboardingView() }
             else { MainTabView() }
         }
-        .animation(.easeInOut, value: store.isLoggedIn)
-        .animation(.easeInOut, value: store.onboardingCompleted)
+        .tint(Color.accentColor)
+        .animation(.easeInOut(duration: 0.25), value: store.isLoggedIn)
+        .animation(.easeInOut(duration: 0.25), value: store.onboardingCompleted)
     }
 }
